30afc6d894e2e00918f7dfa4e33dc5074e4b40f3
mab1096 <78513546+mab1096@users.noreply.github.com>
2024-06-20 09:36:19 -0400

    Update script.js - Full Website Update

--- script.js
+++ script.js
@@ -25,3 +25,103 @@ openModal2.addEventListener('click', () => {
 closeModal2.addEventListener('click', () => {
   modal2.close();
 })
+
+
+
+
+/* Modal #3 */
+const modal3 = document.querySelector('#modal3');
+const openModal3 = document.querySelector('.open-button3');
+const closeModal3 = document.querySelector('.close-button3');
+
+openModal3.addEventListener('click', () => {
+  modal3.showModal();
+})
+
+closeModal3.addEventListener('click', () => {
+  modal3.close();
+})
+
+
+/* Modal #4 */
+const modal4 = document.querySelector('#modal4');
+const openModal4 = document.querySelector('.open-button4');
+const closeModal4 = document.querySelector('.close-button4');
+
+openModal4.addEventListener('click', () => {
+  modal4.showModal();
+})
+
+closeModal4.addEventListener('click', () => {
+  modal4.close();
+})
+
+/* Modal #5 */
+const modal5 = document.querySelector('#modal5');
+const openModal5 = document.querySelector('.open-button5');
+const closeModal5 = document.querySelector('.close-button5');
+
+openModal5.addEventListener('click', () => {
+  modal5.showModal();
+})
+
+closeModal5.addEventListener('click', () => {
+  modal5.close();
+})
+
+/* Modal #6 */
+const modal6 = document.querySelector('#modal6');
+const openModal6 = document.querySelector('.open-button6');
+const closeModal6 = document.querySelector('.close-button6');
+
+openModal6.addEventListener('click', () => {
+  modal6.showModal();
+})
+
+closeModal6.addEventListener('click', () => {
+  modal6.close();
+})
+
+
+/* Modal #7 */
+const modal7 = document.querySelector('#modal7');
+const openModal7 = document.querySelector('.open-button7');
+const closeModal7 = document.querySelector('.close-button7');
+
+openModal7.addEventListener('click', () => {
+  modal7.showModal();
+})
+
+closeModal7.addEventListener('click', () => {
+  modal7.close();
+})
+
+
+
+/* Modal #8 */
+const modal8 = document.querySelector('#modal8');
+const openModal8 = document.querySelector('.open-button8');
+const closeModal8 = document.querySelector('.close-button8');
+
+openModal8.addEventListener('click', () => {
+  modal8.showModal();
+})
+
+closeModal8.addEventListener('click', () => {
+  modal8.close();
+})
+
+
+
+/* Modal #9 */
+const modal9 = document.querySelector('#modal9');
+const openModal9 = document.querySelector('.open-button9');
+const closeModal9 = document.querySelector('.close-button9');
+
+openModal9.addEventListener('click', () => {
+  modal9.showModal();
+})
+
+closeModal9.addEventListener('click', () => {
+  modal9.close();
+})
